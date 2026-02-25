5ee4006b1a8040ef171aa8eba7f2c2087c193e98
Sidhanth-WI <sidhanth.naik@wissen.com>
2026-02-25 14:24:13 +0530

    Using junit to test custom status code

---
+++ spring-junit/target/classes/data.sql
@@ -0,0 +1,7 @@
+insert into EMPLOYEE values(25,'Rajesh');
+insert into EMPLOYEE values(35,'Eshwar');
+insert into EMPLOYEE values(45,'Kamlesh');
+insert into EMPLOYEE values(55,'Sarthak');
+insert into EMPLOYEE values(65,'Sai');
+insert into EMPLOYEE values(75,'Rahul');
+insert into EMPLOYEE values(85,'Aaryan');
